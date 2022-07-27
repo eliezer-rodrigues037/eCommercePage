ac043d22ac00a71dffade2acae43d16cef5efcfc
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2022-07-27 18:19:20 -0300

    Created react app with Vite.

--- API/src/index.js
+++ API/src/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var multer = require("multer");
+var upload = multer();
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static("public"));
 
 //Importa todas as controllers.
 require("./app/controllers/index.js")(app);
